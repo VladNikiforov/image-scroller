7d6a7ea0fdc72717a80e68b615b4804a2a09c141
Vlad Nikiforov <vlad40nikiforov@gmail.com>
2024-08-23 22:14:44 +0300

    update

--- script.js
+++ script.js
@@ -10,3 +10,35 @@ scrollers.forEach((scroller) => {
     scrollerInner.appendChild(duplicatedItem)
   })
 })
+
+const Bill = document.getElementById('Bill')
+
+const images = ['images/BillCipher.png', 'images/brokenTV.png', 'images/art.png', 'images/binary.png', 'images/brokenTV.png', 'images/art.png', 'images/binary.png']
+
+let currentIndex = 0
+
+function glitchEffect() {
+  Bill.src = images[currentIndex]
+
+  setTimeout(glitchEffect, currentIndex == 0 ? 250 : 100)
+
+  currentIndex = (currentIndex + 1) % images.length
+}
+
+glitchEffect()
+
+const Water = document.getElementById('Water')
+
+const elements = ['images/water.png', 'images/fire.png', 'images/earth.png', 'images/wind.png', 'images/leaf.png']
+
+let currentElement = 0
+
+function switchImgs() {
+  Water.src = elements[currentElement]
+
+  setTimeout(switchImgs, 1000)
+
+  currentElement = (currentElement + 1) % elements.length
+}
+
+switchImgs()
